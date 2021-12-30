9f3de537ef981e5ae365ba4979eeefd0082ae16f
taeryu0627 <taeryu0627@naver.com>
2021-12-30 21:57:14 +0900

    서버 설정하기

---
+++ src/controller/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import schoolController from "./school.controller";
+const router = express.Router();
+
+router.use("/schools", schoolController);
+
+export default router;
